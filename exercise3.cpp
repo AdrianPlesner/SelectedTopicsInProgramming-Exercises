e99daed0bd7743be95c7d92322d67e3e9b77e9c0
AdrianPlesner <Adrian.plesner@gmail.com>
2022-02-16 12:27:35 +0100

    Changes by lasse

--- exercise3.cpp
+++ exercise3.cpp
@@ -12,11 +12,12 @@
 
 using namespace std;
 
-vector<string> read_wordle_js(istream&& is)
+vector<string> read_wordle_js(istream&& is) //is er en Base class for char input streams.
 {
 
     vector<string> words;
     auto word = string{};
+
     while (is) { // check for EOF and potential I/O errors
         if (is.peek() == '"') { // look for an opening quote
             is >> quoted(word); // read the word in quotes and remove quotes
@@ -27,10 +28,12 @@ vector<string> read_wordle_js(istream&& is)
         } else
             is.get(); // skip one character
     }
-    sort(begin(words), end(words));
-    words.erase(unique(begin(words), end(words)), words.end());
+    sort(begin(words), end(words)); // Sortere elementerne i ascending rækkefølge
+    words.erase(unique(begin(words), end(words)), words.end()); //Sletter dublicate ord?
     return words;
 }
+
+// Filter the database by removing words which do not match the guess pattern from the puzzle.
 vector<string> filter(vector<string>&& words, const string& pattern)
 {
     auto i {0}, j{0}; //Integers, der bliver initialiseret til 0
@@ -38,22 +41,28 @@ vector<string> filter(vector<string>&& words, const string& pattern)
     while(i < pattern.size()){ // For hvert i, så længe i er mindre en længden af pattern
         try {
             switch (c) {
-                case '-':
-                    i++;
+                case '-': //The character is absent, og ord der indeholder denne character er derfor ikke relevant.
+                    i++; // Hvis strengen nu er "-a-r-ise, og man har det første element, så bliver der peget på "-", men vi skal bruge a
+                    //remove_if: removes elements from a sequence using a predicate. 3 arguments
                     words.erase(remove_if(words.begin(), words.end(),
                                           [=](const string &s) mutable {
                                               return s.find(pattern[i]) != string::npos;
-                                          }), words.end());
+                                          }), words.end()); // Lambda-funktion, der returnere true hvis et ord indeholder denne character
                     break;
-                case '+':
-                    i++;
+                case '+': // The character is present, but not in the right place
+                    i++; // Hvis strengen nu er "+a+r+ise, og man har det første element, så bliver der peget på "-", men vi skal bruge a
+                    //remove_if: removes elements from a sequence using a predicate. 3 arguments
                     words.erase(remove_if(words.begin(), words.end(),
                                           [=](const string &s) mutable {
+                                              // Returnere true for ord, der enten ikke indeholder bogstavet, eller indeholder bogstavet, men i den specifikke position
                                               return (s.find(pattern[i]) == string::npos) || (s[j] == pattern[i]);
                                           }), words.end());
                     break;
-                default:
+                default: // The character is present and in the right place
+                    //remove_if: removes elements from a sequence using a predicate. 3 arguments
+                    //Der er ingen i++;, bogstaver som er på det rette sted, ikke har et - eller + prefix.
                     words.erase(remove_if(words.begin(), words.end(),
+                                          //Lambda-funktion, der returnere true, hvis denne character ikke er i den specifikke posistion
                                           [=](const string &s) mutable {
                                               return s[j] != pattern[i];
                                           }), words.end());
@@ -154,13 +163,13 @@ void suggest(vector<string>& words){
 // Main function which asks the user for the guess pattern, applies the filter, displays one of the remaining valid words and asks for the next guess-pattern.
 int main()
 {
-    std::ifstream strem("wordle.js");
-    if (!strem.is_open())
+    std::ifstream strem("wordle.js"); // Loader databasen med ord
+    if (!strem.is_open()) //Checker om den fejlede i at åbne databasen.
         std::cout << "failed to open " << "wordle.js" << '\n';
     const auto database = read_wordle_js(move(strem));/* read the database from the javascript file */
     auto words = database; // make a copy of the database
     auto guess = string{}; // allocate memory to store guess patterns
-    while (words.size()>1) {
+    while (words.size()>1) { // Et loop der kører så længe at listen af ord er større end 1.
         cout << words.size() << " words remaining\n";
         bool good = true;
         do {
