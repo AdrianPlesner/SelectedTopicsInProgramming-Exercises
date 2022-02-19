cc2da7e69212ed46e751efd45a8fc4f30f856472
Adrian Plesner <adrian.plesner@gmail.com>
2022-02-19 15:28:50 +0100

    Self Study 1

---
+++ NumberSeries.h
@@ -0,0 +1,14 @@
+//
+// Created by adrian on 2/18/22.
+//
+
+#ifndef STP_NUMBERSERIES_H
+#define STP_NUMBERSERIES_H
+
+
+class NumberSeries {
+
+};
+
+
+#endif //STP_NUMBERSERIES_H
---
+++ dynamic_NumberSeries.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adrian on 2/18/22.
+//
+
+#include "dynamic_NumberSeries.h"
